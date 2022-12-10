c85cf28bc8e42cfa4abd19f6754e6a3c6700baa6
atalaykavak <atalay80@hotmail.com>
2022-12-10 11:40:07 -0500

    Person

---
+++ src/week11_review/Person.java
@@ -0,0 +1,64 @@
+package week11_review;
+
+public class Person {
+
+    private String name;
+    private int age;
+    private char gender;
+
+
+    public Person(String name, int age, char gender) {
+        setName(name);
+        setAge(age);
+        setGender(gender);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name.isEmpty() || name == null) {
+
+
+            System.out.println("invalid name");
+            System.exit(1);
+        }
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+
+        if (age <= 0 || age >= 150) {
+            System.out.println("invalid name");
+            System.exit(1);
+        }
+
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+
+    public void setGender(char gender) {
+        if (gender != 'F' || gender != 'M') {
+            System.out.println("Invalid gender");
+            System.exit(1);
+        }
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "name='" + name + '\'' +
+                        ", age=" + age +
+                        ", gender=" + gender;
+    }
+}
